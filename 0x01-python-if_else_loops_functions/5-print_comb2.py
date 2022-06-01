dba8212d5b4615641f31424a5ca36dca0de6bd11
Alatise Oluwaseun <holynation667@gmail.com>
2022-06-01 03:25:04 +0100

    fixed bug

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,2 +1,5 @@
 #!/usr/bin/python3
-print(", ".join("{:02d}").format(i) for i in range(0, 100))
+for i in range(0, 100):
+        print("{:02d}".format(i), end='')
+        if i != (100 - 1):
+            print(", ", end='')
