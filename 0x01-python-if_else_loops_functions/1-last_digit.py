fc5b710a2c321cc525dfbf4162fa41d287e2ae8a
Alatise Oluwaseun <holynation667@gmail.com>
2022-06-01 03:09:39 +0100

    fixed indentation bug

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-tempNum = number
+lastDigit = number
 
 if number < 0:
-	tempNum = number * -1
+	lastDigit = number % (-10)
+else:
+	lastDigit = number % 10
 
-lastDigit = tempNum % 10
 result = "Last digit of {:d} is {:d} and is {}"
-
 if lastDigit > 5:
     result = result.format(number, lastDigit, "greater than 5")
 elif lastDigit == 0:
