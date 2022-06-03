b19b9ad40fc5b435a5290ff8283dbb57d5b6e8df
Alatise Oluwaseun <holynation667@gmail.com>
2022-06-03 05:29:46 +0100

    fix: correct a mis-spelled var

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     from sys import argv
     userArgs = argv[1:] # ignoring the filename with idx 0
-    size = len(userin)
+    size = len(userArgs)
     print("{:d} {:s}{:s}".
           format(size,
                  "arguments" if (size) is not 1 else "argument",
