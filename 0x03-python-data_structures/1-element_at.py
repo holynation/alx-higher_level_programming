0c970b3056af10c32efcd9e94ea06c20cd228288
Alatise Oluwaseun <holynation667@gmail.com>
2022-06-06 23:45:12 +0100

    fix: error

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,4 +3,6 @@ def element_at(my_list, idx):
     listCount = len(my_list)
     if idx < 0 or idx > listCount:
         return None
-    return my_list[idx]
+    for i in range(0, listCount):
+        if i == idx:
+            return my_list[idx]
