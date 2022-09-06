45443f5964b6a19f637cf1f246b98b1c9cc30987
Alatise Oluwaseun <holynation667@gmail.com>
2022-09-06 16:23:50 +0100

    fix: Adjusted the styling

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -15,11 +15,11 @@ module.exports = class Rectangle {
   }
 
   rotate () {
-    let tmp = this.width;
+    const tmp = this.width;
     this.width = this.height;
     this.height = tmp;
   }
-  
+
   double () {
     this.width *= 2;
     this.height *= 2;
