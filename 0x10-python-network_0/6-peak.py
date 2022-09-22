260aa47fe8c61fb1799ca8785399f0ae5bc9708b
Alatise Oluwaseun <holynation667@gmail.com>
2022-09-22 14:47:47 +0100

    fix: code error in oythoin

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -11,21 +11,22 @@ def find_peak(list_of_integers):
     if len(list_of_integers) == 0:
         return None
 
-    l = list_of_integers
-    beg = 0
+    temp_list_integers = list_of_integers
+    start = 0
     end = len(l)-1
 
-    if l[beg] > l[beg+1]:
-        return l[beg]
-    if l[end] > l[end-1]:
-        return l[end]
+    if temp_list_integers[start] > temp_list_integers[start+1]:
+        return temp_list_integers[start]
+    if temp_list_integers[end] > temp_list_integers[end-1]:
+        return temp_list_integers[end]
 
-    mid = (beg+end)//2
-    if l[mid-1] < l[mid] and l[mid+1] < l[mid]:
-        return l[mid]
-    if l[mid] < l[mid-1]:
-        return find_peak(l[beg:mid+1])
-    elif l[mid] < l[mid+1]:
-        return find_peak(l[mid:end+1])
+    mid = (start+end)//2
+    if temp_list_integers[mid-1] < temp_list_integers[mid] and
+    temp_list_integers[mid+1] < temp_list_integers[mid]:
+        return temp_list_integers[mid]
+    if temp_list_integers[mid] < temp_list_integers[mid-1]:
+        return find_peak(temp_list_integers[start:mid+1])
+    elif temp_list_integers[mid] < temp_list_integers[mid+1]:
+        return find_peak(temp_list_integers[mid:end+1])
     else:
-        return l[beg]
+        return temp_list_integers[start]
