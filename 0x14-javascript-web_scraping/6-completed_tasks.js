832dcb0190e71c61904cefcba429a8ea8de5c8c5
Alatise Oluwaseun <holynation667@gmail.com>
2022-10-19 00:10:33 +0100

    fix: semistandard bug

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -7,9 +7,9 @@ request(url, function (err, response, body) {
   if (err) {
     console.log(err);
   } else if (response.statusCode === 200) {
-    let dic = {};
-    let tasks = JSON.parse(body);
-    for (let i in tasks) {
+    const dic = {};
+    const tasks = JSON.parse(body);
+    for (const i in tasks) {
       if (tasks[i].completed) {
         if (dic[tasks[i].userId] === undefined) {
           dic[tasks[i].userId] = 1;
